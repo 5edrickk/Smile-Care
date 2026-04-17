b577efed072188865652b93d281cbdc89a4719eb
Sedrick Zahn <sezahn@icloud.com>
2026-04-17 19:32:36 -0400

    feat: ajout fichiers view, controller, model pour fonctionnalite Rendez Vous de base

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProduitController;
 use Illuminate\Support\Facades\Route;
+
+// Route::controller(NomDuControleur::class)->group(function() { 
+//     Route::get('/Route', 'MethodeDuControleur')->name('NomDeLaRoute'); 
+//     });
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +20,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::controller(RendezVousController::class)->group(function() {
+    Route::get('/rendezvous', 'index')->name('rendezvous');
 });
 
 require __DIR__.'/auth.php';
