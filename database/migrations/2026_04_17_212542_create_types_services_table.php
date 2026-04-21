6a25cce1f6be105283f6515387daf99707278061
bern1408 <berncuzcuz1408@gmail.com>
2026-04-21 10:55:28 -0400

    Max char 500 (description types_services)

--- database/migrations/2026_04_17_212542_create_types_services_table.php
+++ database/migrations/2026_04_17_212542_create_types_services_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('types_services', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description')->nullable();
+            $table->string('description', 500)->nullable();
         });
     }
 
