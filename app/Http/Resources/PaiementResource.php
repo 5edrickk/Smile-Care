0a37db4ebe2e5b297ca03bede292bb9ee9cbac38
Abdem80 <dembeleabdullah1@gmail.com>
2026-04-22 21:23:34 -0400

    feat: créer PaiementResource pour formatage JSON API

---
+++ app/Http/Resources/PaiementResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PaiementResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'             => $this->id,
+            'montant'        => $this->montant,
+            'id_rendez_vous' => $this->id_rendez_vous,
+
+            // retourne le nom au lieu de juste l'ID
+            'etat'           => $this->etatPaiement->name ?? null,
+            'type'           => $this->typePaiement->name ?? null,
+        ];
+    }
+}
