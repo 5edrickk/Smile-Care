3e4c31e5a07f62359860371ac3941cc4cdc4701f
CCcMoiadl <adlcube17@gmail.com>
2026-04-29 13:10:19 -0400

    Ajustement au footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,4 @@
 <div class="w-[100%] h-[25px] bg-[#006E8C]
-            sticky bottom-0
             flex justify-between
             px-4">
     <pre> Smile Care™</pre><pre class="text-white">Bernardo Goncalves Da Cruz - Alexandre Doe-Langevin - Sèdrick Zahn Tsoucas - Abdoulaye Dembele</pre>
