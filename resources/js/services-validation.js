81d4d79c42c9905caf114a0c2dd4b709ab78b926
bern1408 <berncuzcuz1408@gmail.com>
2026-05-05 11:01:42 -0400

    validation js fonctionnelle

--- resources/js/services-validation.js
+++ resources/js/services-validation.js
@@ -1,49 +1,61 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementsByClassName("form-services");
-    for (let i = 0, l = form.length; i < l; i++){
-        if (!form[i]) return;
+    const form = document.getElementById("form-services");
+    //for (let i = 0, l = form.length; i < l; i++){
+        if (!form) return;
 
-        form[i].addEventListener("submit", function (e) {
+        form.addEventListener("submit", function (e) {
             let valide = true;
-
             reinitialiserErreurs();
 
-            const services_name = document.getElementsByClassName("services-name").value;
-            if (!services_name[i]) {
-                afficherErreur("services-name", "Veuillez saisir le nom du service.");
+            const services_name = document.getElementById("service_name").value;
+            if (!services_name) {
+                afficherErreur("service_name", "Veuillez saisir le nom du service");
+                valide = false;
+            }
+            else{
+                if(services_name.length > 255){
+                    afficherErreur("service_description", "Le nom ne peut pas dépasser 255 caractères");
+                    valide = false;
+                }
+            }
+
+            const services_categorie = document.getElementById("service_categorie").value;
+            if (!services_categorie) {
+                afficherErreur("service_categorie", "Veuillez sélectionner la catégorie du service");
                 valide = false;
             }
 
-            const services_categorie = document.getElementById("services-categorie").value;
-            if (!services_categorie[i]) {
-                afficherErreur("service-categorie", "Veuillez sélectionner la catégorie du service.");
-                valide = false;
-            }
-
-            const service_duree = document.getElementById("services-duree").value;
-            if (!service_duree[i]) {
-                afficherErreur("service-duree", "Veuillez saisir la duree du service.");
+            const service_duree = document.getElementById("service_duree").value;
+            if (!service_duree) {
+                afficherErreur("service_duree", "Veuillez saisir la duree du service");
                 valide = false;
             }else {
-                if (isNaN(service_duree[i])) {
+                if (isNaN(service_duree)) {
                     afficherErreur(
-                        "service-duree",
-                        "Veuillez saisir un chiffre.",
+                        "service_duree",
+                        "La durée doit être un chiffre",
                     );
                     valide = false;
-                } else if (!/\b([1-9]|[0-2][0-4])\b/.test(service_duree[i])) {
+                } else if (!/\b([1-9]|[0-2][0-4])\b/.test(service_duree)) {
                     afficherErreur(
-                        "service-duree",
-                        "Veuillez saisir un chiffre situé entre 1 et 24." +
-                            " - " +
-                            service_duree[i],
+                        "service_duree",
+                        "La durée doit se situer entre 1 et 24",
                     );
+                    valide = false;
+                }
+            }
+
+            const service_description = document.getElementById("service_description").value;
+            if (service_description) {
+                if(service_description.length > 255){
+                    afficherErreur("service_description", "La description ne peut pas dépasser 255 caractères");
                     valide = false;
                 }
             }
 
             if (!valide) {
                 e.preventDefault();
+                console.log("ERROR");
             }
         });
 
@@ -61,5 +73,5 @@ document.addEventListener("DOMContentLoaded", function () {
                 el.classList.add("hidden");
             });
         }
-    }
+
 });
--- resources/views/services/servicesCreate.blade.php
+++ resources/views/services/servicesCreate.blade.php
@@ -11,29 +11,33 @@
                 <div class="bg-cyan-500 px-6 py-5 text-white font-bold">
                     Ajouter un service
                 </div>
-                <form action="{{ route('services.store') }}" method="POST" class="form-services space-y-4 px-6 py-6 shadow-2xl">
+                <form action="{{ route('services.store') }}" method="POST" id="form-services" class="space-y-4 px-6 py-6 shadow-2xl">
                     @csrf
                     <label for="sname" class="text-sm font-medium">Nom du service<span class="text-red-500">*</span></label>
-                    <input type="text" name="name" required maxlength="255"
-                        class="services-name mt-1 w-full rounded-md border border-gray-300 px-3 py-2 focus:ring-1 focus:ring-cyan-400">
+                    <input type="text" name="name" id="service_name"
+                        class="mt-1 w-full rounded-md border border-gray-300 px-3 py-2 focus:ring-1 focus:ring-cyan-400">
+                    <p id="erreur-service_name" class="hidden text-red-700 text-sm"></p>
 
                     <label for="categorie" class="text-sm font-medium">Catégorie<span class="text-red-500">*</span></label>
-                    <select type="text" name="categorie" required
-                        class="services-categorie mt-1 w-full rounded-md border border-gray-300 px-3 py-2 focus:ring-1 focus:ring-cyan-400">
+                    <select type="text" name="categorie" id="service_categorie"
+                        class="mt-1 w-full rounded-md border border-gray-300 px-3 py-2 focus:ring-1 focus:ring-cyan-400">
                         @foreach ($id_type as $categorie)
                             <option value={{ $categorie->id }}>
                                 {{ old('id_type') == $categorie->id ? 'selected' : '' }}{{ $categorie->name }}
                             </option>
                         @endforeach
                     </select>
+                    <p id="erreur-service_categorie" class="hidden text-red-700 text-sm"></p>
 
                     <label for="duree" class="text-sm font-medium">Durée<span class="text-red-500">*</span></label>
-                    <input type="number" name="duree" required min="1"
-                        class="services-duree mt-1 w-full rounded-md border border-gray-300 px-3 py-2 focus:ring-1 focus:ring-cyan-400">
+                    <input type="text" name="duree" id="service_duree"
+                        class="mt-1 w-full rounded-md border border-gray-300 px-3 py-2 focus:ring-1 focus:ring-cyan-400">
+                    <p id="erreur-service_duree" class="hidden text-red-700 text-sm"></p>
 
                     <label for="description" class="text-sm font-medium">Description</label>
-                    <textarea type="text" name="description" maxlength="255"
-                        class="services-description mt-1 w-full rounded-md border border-gray-300 px-3 py-2 focus:ring-1 focus:ring-cyan-400"></textarea>
+                    <textarea type="text" name="description" id="service_description"
+                        class="mt-1 w-full rounded-md border border-gray-300 px-3 py-2 focus:ring-1 focus:ring-cyan-400"></textarea>
+                    <p id="erreur-service_description" class="hidden text-red-700 text-sm m-0"></p>
 
                     <div class="flex items-center justify-end gap-3 pt-2">
                         <a href="{{ route('services') }}"
